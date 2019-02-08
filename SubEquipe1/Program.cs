db3f185addeab817ef87105df00f92cf2a0aca9d
logonrmlocal <logonrmlocal@L2202MICRO05>
2019-02-07 22:03:38 -0200

    Commit inicial

---
+++ SubEquipe1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SubEquipe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            }
+        }
+    }
+}
